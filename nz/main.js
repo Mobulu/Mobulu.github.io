e20132cb0932bc4de09d90b61a26cd88f4595703
Mario Lehmeyer <mario.lehmeyer@student.uibk.ac.at>
2022-04-15 14:11:13 +0200

    Popups an alle etappen gehängt

--- nz/main.js
+++ nz/main.js
@@ -12,6 +12,15 @@ L.marker([-38.136944, 176.250833]).addTo(map)
     .openPopup();
 
     for (let etappe of ETAPPEN) {
+        let popup = `
+      <h3>${etappe.titel} (Etappe ${etappe.nr})</h3>
+      <ul>
+      <li>geogr. Länge: ${etappe.lng}</li>
+      <li>geogr. Breite: ${etappe.lat}</li>
+      <li><a href="${etappe.wikipedia}">Link zur Wikipediaseite</a></li>
+      <li><a href="https://${etappe.github}.github.io/nz/">Link zur Etappenseite</a></li>
+  </ul>
+  `;
         //console.log(etappe);
-        L.marker([etappe.lat, etappe.lng]).addTo(map);
+        L.marker([etappe.lat, etappe.lng]).addTo(map).bindPopup(popup);
     }
