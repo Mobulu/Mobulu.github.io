d57d2af6bd57e88720e8d15df649c2edd50625b3
Mobulu <102307924+Mobulu@users.noreply.github.com>
2022-04-15 13:46:19 +0200

    Add files via upload

---
+++ nz/main.js
@@ -0,0 +1,12 @@
+/* Neuseelandreise Skript */
+// einzeiliger Kommentar
+
+var map = L.map('map').setView([-38.136944, 176.250833], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([-38.136944, 176.250833]).addTo(map)
+    .bindPopup('<h3>Rotorua</h3>')
+    .openPopup();
